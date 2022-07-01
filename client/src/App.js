85f6218f993d67fde203882d4c0fbc5b4e11167a
Nihal Sarwar <nihalsarwar17@gmail.com>
2022-07-01 23:25:53 +0500

    Styled home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import data from "./data";
 
 function App() {
   return (
     <div>
-     <header>
-     <a href="/">amazona</a>
-     </header>
-     <main>
-      List Products
-     </main>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>
+        <h1>Featured Products</h1>
+
+        <div className="products">
+          {data.products.map((product) => {
+            return (
+              <div className="items" key={product.slug}>
+
+                <a href={`/item/${product.slug}`}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  style={{ height: "320px" }}
+                ></img>
+                </a>
+
+                <div className="items-info">
+
+                <a href={`/item/${product.slug}`}>
+                  <p>{product.name}</p>
+                  </a>
+
+                  <p>Price: <strong>${product.price}</strong></p>
+                  <button>Add to cart</button>
+                </div>
+                
+              </div>
+            );
+          })}
+        </div>
+      </main>
     </div>
   );
 }
