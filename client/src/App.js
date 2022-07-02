0d0180f1ab4af8b4da42e86461eda82971d12a21
Nihal Sarwar <nihalsarwar17@gmail.com>
2022-07-02 17:10:41 +0500

    Added React-Bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,38 @@ import "./App.css";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
 
   return (
     <BrowserRouter>
-    <div>
+    <div className="d-flex flex-column site-container">
       <header>
+      {/* <Navbar bg="dark" color="light" varient="dark">
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand >Amazona</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar> */}
         <Link to="/">Amazona</Link>
       </header>
       <main>
+        {/* <Container> */}
         <Routes>
       
           <Route path="/" element={<HomeScreen/>} />
           <Route path="/item/:slug" element={<ProductScreen/>}/>
         </Routes>
-      
+      {/* </Container> */}
       </main>
+
+      <footer>
+        <div className="text-center">All right reserved</div>
+      </footer>
     </div>
   </BrowserRouter>
   );
